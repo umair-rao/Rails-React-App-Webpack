dcc50d473947a29e4603a028a2aba95a00e6fadf
umair-rao <umairmuqeem@gmail.com>
2024-03-05 12:02:45 +0500

    Add routes

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Greeting from './Greeting.js'
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
+
 
 const App = () => {
   return (
     <div>
-        <Greeting />
+      <Router>
+      <Routes>
+        <Route path="/" element={<Greeting />} />
+      </Routes>
+    </Router>
+
     </div>
   )
 }
