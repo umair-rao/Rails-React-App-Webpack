644bf44678a96c5f4f1def170373de951f2b2096
umair-rao <umairmuqeem@gmail.com>
2024-03-08 17:29:06 +0500

    Add routes and delete extra files

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,17 +2,17 @@ import "@hotwired/turbo-rails";
 import "./controllers";
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import Greeting from "./components/Greeting";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <Router>
       <h1>Greetings from Umair :</h1>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Greeting />} />
       </Routes>
     </Router>
   </Provider>
